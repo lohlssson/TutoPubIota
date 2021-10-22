37a8ee4a7b223a2b1d2ab3e3fa843e525a3605f5
laurent ohlsson <82362766+lohlssson@users.noreply.github.com>
2021-10-22 11:15:58 +0200

    Update 20211021Tuto5SendPaiement.py

--- IotaTuto/20211021Tuto5SendPaiement.py
+++ IotaTuto/20211021Tuto5SendPaiement.py
@@ -1,4 +1,4 @@
-#source code : https://legacy.docs.iota.org/docs/core/1.0/tutorials/js
+#source code : https://legacy.docs.iota.org/docs/core/1.0/tutorials/python
 from iota import Iota
 from iota import ProposedTransaction
 from iota import Address
