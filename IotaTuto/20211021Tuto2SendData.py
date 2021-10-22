5f1ae19bd0031df69cf2ed6301de3f05a0d8f5f6
laurent ohlsson <82362766+lohlssson@users.noreply.github.com>
2021-10-22 11:15:00 +0200

    Update 20211021Tuto2SendData.py

--- IotaTuto/20211021Tuto2SendData.py
+++ IotaTuto/20211021Tuto2SendData.py
@@ -1,4 +1,4 @@
-#source code : https://legacy.docs.iota.org/docs/core/1.0/tutorials/js
+#source code : https://legacy.docs.iota.org/docs/core/1.0/tutorials/python
 from iota import Iota, TryteString, Address, Tag, ProposedTransaction
 from pprint import pprint
 
